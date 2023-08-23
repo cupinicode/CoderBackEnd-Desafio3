a526b7a4e8518fa9e027fa2ef14455b78894517f
Jorge Cupini <jorge.cupini@bue.edu.ar>
2023-08-23 20:07:56 -0300

    Corregidas llamadas a metodos de clase

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
-const express = require("express");
+const express = require("express"); //importo EXPRESS
 
 const app = express() //Defino "app" como una instancia del módulo express
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true})); //Para que el serivodr pueda procesar URLs con QUery Params
 
-const ProductManager = require("./productManager.js")
+const ProductManager = require("./productManager.js") //Importo la clase ProductManager
 
 const manag = new ProductManager("./productos.txt") // Creo una instancia de la clase ProductManager 
 
-const products = manag.getProducts()//Llamo al método de ProductManager que lee el archivo, me devuelve todos los productos y lo cargo en un array
 
-app.get('/products', (req, res) => {
+app.get('/products', (req, res) => { //endpoint
     const limit = req.query.limit
+    const products = manag.getProducts()//Llamo al método de ProductManager que lee el archivo, me devuelve todos los productos y lo cargo en un array
     if (limit) { // Verifico si recibí el parámetro limit en un query param
-        return res.send(productos.slice(0, parseInt(limit, 10))) // extrae los primeros "n" elementos del array
+        return res.send(products.slice(0, parseInt(limit, 10))) // extrae los primeros "n" elementos del array
     } // (el return está para obligar a salir de la función)
     res.send(products)
 })
 
-app.get('/products/:pid', (req, res) => {
+app.get('/products/:pid', (req, res) => {  //endpoint
     const pid = parseInt(req.params.pid,10)
-    const prod = products.find(({ id }) => id === pid) //Uso desestructuración de objetos para obtener sólo el ID
-    res.send(prod) //Devuelvo el primer producto con la ID pasada por parametro a la ruta
+    res.send(manag.getProductById(pid)) //Devuelvo el primer producto con la ID pasada por parametro a la ruta
 })
 
 app.listen(8080, () => console.log("Servidor escuchando en puerto 8080"))
